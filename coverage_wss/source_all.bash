13d88aa23915157757c587938970e6e889eca94d
Elliott White <43571285+ElliWhite@users.noreply.github.com>
2019-05-27 12:12:01 +0100

    Delete source_all.bash

--- coverage_wss/source_all.bash
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-TURTLEBOT_DRIVERS_WS=~/turtlebot2_wss/turtlebot_ws
-TURTLEBOT_SIMULATION_WS=~/turtlebot2_wss/turtlebot_simulation
-TURTLEBOT_INTERACTION_WS=~/turtlebot2_wss/turtlebot_interaction
-TURTLEBOT_NAVIGATION_WS=~/turtlebot2_wss/turtlebot_navigation_ws
-source $TURTLEBOT_DRIVERS_WS/devel/setup.bash --extend
-source $TURTLEBOT_SIMULATION_WS/devel/setup.bash --extend
-source $TURTLEBOT_INTERACTION_WS/devel/setup.bash --extend
-source $TURTLEBOT_NAVIGATION_WS/devel/setup.bash --extend
